709c03ddb71e7198b046f0c3f5299a796bce4595
Harsh Wardhan <wardhan071@gmail.com>
2024-03-08 12:47:04 +0530

    first commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,1 +1,16 @@
-console.log('')
+const login = async (req, res) => {
+  res.send('Fake Login/Register/Signup Route');
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: 'Hello, Harsh Wardhan',
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
