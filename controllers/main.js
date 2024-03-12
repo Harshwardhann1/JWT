c2f4d4dfc5053ca7dbb3b4d65a4aca4251d20d54
Harsh Wardhan <112763662+Harshwardhann1@users.noreply.github.com>
2024-03-12 16:26:31 +0530

    Update main.js

--- controllers/main.js
+++ controllers/main.js
@@ -19,8 +19,6 @@ const login = async (req, res) => {
   //just for demo, normally provided by DB!!!!
   const id = new Date().getDate()
 
-  // try to keep payload small, better experience for user
-  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   })
